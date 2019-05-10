d91ebf887bd0733667566fa1d1068644e9065b98
Narcos91 <giulio.biz92@yahoo.it>
2019-05-10 17:17:19 +0200

    Aggiunta della funzionalità di creazione/inserimento di un nuovo utente

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+const express = require('express');
+// eslint-disable-next-line new-cap
+const router = express.Router();
+const {check} = require('express-validator/check');
+
+const User = require('../models/users');
+
+
+router.post('/', [
+  check('name').exists(),
+  check('surname').exists(),
+  check('email').isEmail(),
+  check('age').isInt(),
+],
+function(req, res) {
+  const newUser = new User(req.body);
+  newUser.save(function(err) {
+    if (err) return res.status(500).json({error: err});
+    res.status(201).json(newUser);
+  });
+});
+
+module.exports = router;
