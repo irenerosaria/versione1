e54486c7e4f88fb436c97ac36ac946e922bb2f1c
Narcos91 <giulio.biz92@yahoo.it>
2019-05-10 15:20:02 +0200

    Modifica File App.js

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-//stabiliamo la connessione con il database
+var mongoose = require('mongoose');
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+const routes = require('./routes/index');
+const usersRoutes = require('./routes/users');
+
+
+
 const host = 'localhost';
-const dbName = 'dbUser';
+let dbName = 'User';
 
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb://${host}/${dbName}`);
 
-var db = mongoose.connection;
+
+mongoose.connect('mongodb://'+ host + '/' + dbName, {useNewUrlParser: true});
+
+const db = mongoose.connection;
 db.on('error', function() {
-    console.error('Connection error!')
+  console.error('Connection error!');
 });
 db.once('open', function() {
+
     console.log('DB connection Ready');
+  
+}); 
+
+var app = express();
+app.use(bodyParser.json());
+
+app.use('/', routes);
+app.use('/users', usersRoutes);
+
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
+// Error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+      .json({message: err.message, error: err});
+});
 var User = require('./models/model');
-applicationCache.addEventListener(3001)
-module.exports=app;
+app.listen(3001)
+module.exports = app;
+
